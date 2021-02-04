1cb9210f00c2de1998e5570741916f430babf147
ravigavde <nightr714@gmail.com>
2021-02-04 12:12:01 +0530

    added function file

---
+++ Es6/functions.js
@@ -0,0 +1,31 @@
+// //normal function declaration
+// function greet()
+// {
+//     console.log("hello there !");
+// }
+
+// greet();
+
+// // lets rewrite the same with arrow function
+
+// let greet2 = () => "hello there  from arrow function !"
+
+// console.log( greet2() );
+
+// // now the fat arrow function call is bit different than normal function , in fat arrow function this keyword always points towards the window whereas in normal function that depends who is caling this
+
+// function arguments 
+    // default  
+// function equals(one ,two = 0)
+// {
+//     return( one == two );
+// }
+
+//  console.log( equals(1) );
+    // parameters
+// function withParameters(positionOne, positionTwo)
+// {
+//     console.log("Parameters are "+ positionOne +" "+ positionTwo );
+// }    
+
+// withParameters("A string and a number next ->", 23);
