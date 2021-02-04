cc4c3060ae5526607f4e93fcfe40c4525d9efaec
ravigavde <nightr714@gmail.com>
2021-02-04 14:00:39 +0530

    operators file added

---
+++ Es6/operators.js
@@ -0,0 +1,18 @@
+// // rest operator
+//  // when we dont know how many parameters we are passing to a function, that time we need a rest operator ie  function(...Array)
+ 
+//  function dontKnowParameters(...array)
+//  {
+//     console.log(array);
+//  }
+//  dontKnowParameters(10,20,30,"trying string");
+
+
+//  //spread operator
+//     //it is opposite of the rest operator and we know that we all need to iteratte the array sometimes in any code so
+// let array = [1,2,3,4,5,6,7];
+
+// console.log(...array);
+
+// // or better example is using max function 
+// console.log( Math.max(...array) );
