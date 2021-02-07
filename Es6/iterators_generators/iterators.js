f340cdbff5573dbb6f2590619067a9ab15930a24
ravigavde <nightr714@gmail.com>
2021-02-07 12:56:02 +0530

    added iterators file

---
+++ Es6/iterators_generators/iterators.js
@@ -0,0 +1,21 @@
+//iterators
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+let it = array[Symbol.iterator]();
+
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+
+// while(!(it.next().done))
+// {
+//     console.log( it.next().value  );
+// }
+
