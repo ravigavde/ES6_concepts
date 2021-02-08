6abdf74d7dfa0c111d1fd83139d42d75b8eb0b31
ravigavde <nightr714@gmail.com>
2021-02-08 12:59:27 +0530

    added babel example

---
+++ Es6/babel/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _private = 1200;
+var greet = function greet() {
+  return console.log("hello");
+};
---
+++ Es6/babel/src/main.js
@@ -0,0 +1,2 @@
+let _private = 1200;
+let greet = () => console.log("hello");
